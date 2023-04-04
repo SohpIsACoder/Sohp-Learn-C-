94d0df38ac29ae5b3c3b73430a707b0861b9e903
Sohp <127593883+SohpIsACoder@users.noreply.github.com>
2023-04-05 00:55:15 +0800

    Add files via upload

---
+++ 03 常量.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+//常量的定义方式
+//1、 #define 宏常量
+//2、 const修饰的变量
+
+//1、 #define 宏常量
+#define Day 7
+
+int main() {
+	cout << "一周总共有" << Day << "天" << endl;
+
+	system("pause");
+
+	return(0);
+}
---
+++ 04 整型.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	//短整型
+	short num1 = 32768;
+	
+	//整型
+	int num2 = 10;
+	
+	//长整型
+	int num3 = 10;
+	
+	//长长整型
+	int num4 = 10;
+	
+	cout<<"num1 = "<<num1<<endl;
+	cout<<"num2 = "<<num2<<endl;
+	cout<<"num3 = "<<num3<<endl;
+	cout<<"num4 = "<<num4<<endl;
+	
+	system("pause");
+	
+	return(0); 
+} 
